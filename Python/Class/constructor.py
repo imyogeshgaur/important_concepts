748a178d0a33173e52d688cf83d5f84737435c0d
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-11-03 17:28:57 +0530

    Built a constructor of a class in python

---
+++ Python/Class/constructor.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__ (self,eId,eName,eContact,eSalary):
+        self.employeeName = eName
+        self.employeeId = eId;
+        self.employeeContactNumber = eContact
+        self.employeeSalary = eSalary
+    
+    def printValues(self):
+        print(f"The Employee id is {self.employeeId}.\nThe Employee Name is {self.employeeName}.\nThe Employee Contact Number is {self.employeeContactNumber}.\nThe Employee Salary is {self.employeeSalary}.")
+    
+yogesh =  Employee(10001,"Yogesh Gaur",9868747636,43534.359)
+yogesh.printValues()
