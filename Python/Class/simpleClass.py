ee8da1b0c432c41fd6cd9c80ebc4be0abce30c18
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-11-03 17:28:11 +0530

    Built a basic class in python

---
+++ Python/Class/simpleClass.py
@@ -0,0 +1,17 @@
+class Employee:
+    def getValues(self,eId,eName,eContact,eSalary):
+        self.employeeName = eName
+        self.employeeId = eId;
+        self.employeeContactNumber = eContact
+        self.employeeSalary = eSalary
+
+    def printValues(self):
+        print(f"The Employee id is {self.employeeId}.\nThe Employee Name is {self.employeeName}.\nThe Employee Contact Number is {self.employeeContactNumber}.\nThe Employee Salary is {self.employeeSalary}.")
+
+yogesh = Employee();
+yogesh.getValues(10001,"Yogesh Gaur",9868747636,43534.359)
+yogesh.printValues()
+
+
+
+    
