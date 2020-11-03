4f436de3c3004423fbea5fa29ff4bd600d1145f6
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-11-03 17:29:30 +0530

    Built class Methods in python

---
+++ Python/Class/classMethod.py
@@ -0,0 +1,20 @@
+class Employee:
+    employeeId = 0
+    employeeName = "XYz"
+    employeeContactNumber = 0
+    employeeSalary = 0.0
+
+    @classmethod
+
+    def getValues(cls,eId,eName,eContact,eSalary):
+        cls.employeeId = eId
+        cls.employeeName= eName
+        cls.employeeContactNumber = eContact
+        cls.employeeSalary = eSalary
+
+    def printValue(self):
+          print(f"The Employee id is {self.employeeId}.\nThe Employee Name is {self.employeeName}.\nThe Employee Contact Number is {self.employeeContactNumber}.\nThe Employee Salary is {self.employeeSalary}.")
+    
+yogesh = Employee()
+yogesh.getValues(10001,"Yogesh Gaur",9868747636,43534.359)
+yogesh.printValue()
