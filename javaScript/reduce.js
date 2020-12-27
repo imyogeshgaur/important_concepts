797d873c7cba5763a9929a626ee2534c3a5d6af1
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-27 10:44:36 +0530

    Understand Reduce Function

--- javaScript/reduce.js
+++ javaScript/reduce.js
@@ -2,29 +2,28 @@
 
 //Sum, Average and Product using Reduce Method
 
-const arr = [12,23,34,45,56,67,78,89,90,100]
+const arr = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100];
 
-
-const sum = arr.reduce((accum,currentVal)=>{
-debugger;
-    return accum + currentVal
-})
+const sum = arr.reduce((accum, currentVal) => {
+  debugger;
+  return accum + currentVal;
+});
 
 console.log(sum);
 
-const prod = arr.reduce((accum,currentVal)=>{
-    debugger;
-    return accum * currentVal
-})
+const prod = arr.reduce((accum, currentVal) => {
+  debugger;
+  return accum * currentVal;
+});
 
 console.log(prod);
 
-const average = arr.reduce((accum,currentVal,index,arr)=>{
-    let tot = accum += currentVal
-    if(index === arr.length-1){
-         return tot/arr.length
-    }
-    return tot
-})
+const average = arr.reduce((accum, currentVal, index, arr) => {
+  let tot = (accum += currentVal);
+  if (index === arr.length - 1) {
+    return tot / arr.length;
+  }
+  return tot;
+});
 
 console.log(average);
