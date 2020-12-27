f5a9485aaf462c0cad59af10f48718722ca65898
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-27 15:45:00 +0530

    Understand Bind Method

---
+++ javaScript/bind.js
@@ -0,0 +1,16 @@
+const mybio = {
+    name:"Yogesh",
+    class:"3D",
+    subject:"CSE",
+    call:function(){
+         console.log(`My Name is ${this.name} my class is ${this.class} and my subject is ${this.subject}`);
+    },
+    callName:function(){
+      console.log(`My name is ${this.name}`);
+    }
+}
+
+mybio.callName()
+const biography = mybio.call.bind()
+
+biography()
