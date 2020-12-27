8b742cd92678da958384fd2447694e1926913242
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-27 10:44:51 +0530

    Understand Splice Method

--- javaScript/spliceCRUD.js
+++ javaScript/spliceCRUD.js
@@ -1,26 +1,25 @@
-
 // splice(indextoStart,numberofElementsDeletedAfterStartingIndex,newValues)
 
 // Insert values to Array
 
-const arr = []
+const arr = [];
 
-arr.splice(arr.length,0,"Yogesh","Mukesh","Suresh","Hitesh")
+arr.splice(arr.length, 0, "Yogesh", "Mukesh", "Suresh", "Hitesh");
 
 console.log(arr);
 
 //Update Yogesh To Ramesh
 
-arr.splice(0,1,"Ramesh")
+arr.splice(0, 1, "Ramesh");
 
 console.log(arr);
 
 // Delete Ramesh and Insert Yogesh To End
 
-arr.splice(0,1) 
+arr.splice(0, 1);
 
 console.log(arr);
 
-arr.splice(arr.length,0,"Yogesh")
+arr.splice(arr.length, 0, "Yogesh");
 
 console.log(arr);
