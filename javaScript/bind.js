6535c4d2764edbec4488b39a0ed18fd2ee5ae3ea
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-27 15:45:55 +0530

    Understand Bind Method

--- javaScript/bind.js
+++ javaScript/bind.js
@@ -1,16 +1,18 @@
 const mybio = {
-    name:"Yogesh",
-    class:"3D",
-    subject:"CSE",
-    call:function(){
-         console.log(`My Name is ${this.name} my class is ${this.class} and my subject is ${this.subject}`);
-    },
-    callName:function(){
-      console.log(`My name is ${this.name}`);
-    }
-}
+  name: "Yogesh",
+  class: "3D",
+  subject: "CSE",
+  call: function () {
+    console.log(
+      `My Name is ${this.name} my class is ${this.class} and my subject is ${this.subject}`
+    );
+  },
+  callName: function () {
+    console.log(`My name is ${this.name}`);
+  },
+};
 
-mybio.callName()
-const biography = mybio.call.bind()
+mybio.callName();
+const biography = mybio.call.bind();
 
-biography()
+biography();
